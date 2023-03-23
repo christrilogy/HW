b5a1898061a432c66d3934ea21de77f8e88b1a3c
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-03-23 05:05:13 -0400

    Create num37.cpp

---
+++ num37.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+
+  int product = 0;
+  int num;
+  
+while (product < 100){
+
+  cout << "Enter a number ";
+  cin >> num;
+
+   product= num * 10;
+
+  cout<< product << endl;  
+
+}
+  
+
+
+  return 0;
+}
