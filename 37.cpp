646c5bb28050fd450153967a8ab5d99b5040974e
christrilogy <112494599+christrilogy@users.noreply.github.com>
2023-03-23 05:12:23 -0400

    Create 37.cpp

---
+++ 37.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int product = 0;
+int number;
+
+int main (){
+  int x;
+  for (x =0; x<=1000; x += 10)
+    {
+      cout<< x << endl;
+    }
+
+return 0;
+}
